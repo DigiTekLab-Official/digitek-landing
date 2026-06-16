57ee74a7916290ececbfd700412b1ff0e04323c7
Ameer <ameer.itoa@gmail.com>
2026-06-16 21:46:17 +0530

    any

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,7 +1,9 @@
+const base = 'https://digiteklab.com';
 const pages = ['', 'services', 'portfolio', 'about', 'contact'];
+
 export function GET() {
   const urls = pages
-    .map((p) => `  <url><loc>https://digiteklab.com/${p}</loc></url>`)
+    .map((p) => `  <url><loc>${p ? `${base}/${p}` : base}</loc></url>`)
     .join('\n');
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
